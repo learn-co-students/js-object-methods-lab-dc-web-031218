8d29da458b72cf936f28b055e063170c582c1489
azeelleeza <leezaluncheon@gmail.com>
2018-04-26 10:33:46 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code solution here
+// President class with the following properties set by the constructor: name, politicalParty, yearsInOffice, and homeState.
+
+class President{
+  constructor(name, politicalParty, yearsInOffice,homeState){
+    this.name = name;
+    this.politicalParty = politicalParty;
+    this.yearsInOffice = yearsInOffice;
+    this.homeState = homeState;
+  }
+  // let veto = ()=>"No";
+  veto(){return "NO!";}
+  passBill(){return "You can do that!";}
+  doCharity(){return "I like to help people.";}
+  conductPressInterview(){return "I am proud to be an American.";}
+  sayHi(){return `Hi, my name is ${this.name}. I am from ${this.homeState}. I represent the ${this.politicalParty}s, and was in office ${this.yearsInOffice}.`;}
+}
